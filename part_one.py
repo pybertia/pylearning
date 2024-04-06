b9ab718776af4a91ab1501f18c8690e5348d16a5
pybertia <160273411+pybertia@users.noreply.github.com>
2024-04-06 08:41:47 +0000

    ch02

--- part_one.py
+++ part_one.py
@@ -1,4 +1,43 @@
 # encoding=utf-8
 
-# ch01
-print('Hello, Python!')
+# ch02: Python variables and data types
+message = 'Hello, Python!'
+print(message)
+message = 'Hello, Python Crash Course world!'
+print(message)
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+favarite_language = ' python '
+print(favarite_language.rstrip())
+print(favarite_language.lstrip())
+print(favarite_language.strip())
+
+# ex02-03
+name = 'Eric'
+message = 'Hello ' + name + ', would you like to learn some Python today?'
+print(message)
+
+import this
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
